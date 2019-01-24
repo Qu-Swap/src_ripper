5e5c4b35fe5ff36d921776625418484873665bca
askdfjlas <kaihengd@gmail.com>
2019-01-24 18:53:59 -0500

    course descriptions

--- ripper.py
+++ ripper.py
@@ -20,7 +20,8 @@ def retrieve_info(html, index):
         tokens = html.split()
         return tokens[0] + " " + tokens[1]
     elif index == 2:
-        return re.split("[<>]", html)[2]
+        # Returns the course name along with the name of the element holding the description
+        return re.split("[<>]", html)[2], re.split("[\"]", html)[1]
     elif index == 6:
         if "href" in html:
             return re.split("\"", html)[1].replace(" ", "%20").replace("&amp;", "&")
@@ -47,20 +48,50 @@ def get_courses(link, subjects):
     table = page.findAll("table")[8]
 
     rows = table.findAll("tr")[3:]
+    count = 0
     for row in rows:
         tds = row.findAll("td")
         if len(tds) == numColumns:
             course = {}
             for index in indexes:
                 inner = tds[index].decode_contents().replace("\n", "")
-                course[indexes[index]] = retrieve_info(inner, index)
+
+                if index == 2:
+                    info = retrieve_info(inner, index)
+                    course[indexes[index]] = info[0]
+
+                    # Pretty inefficient to not check a duplicate here, as finding the name takes a while
+                    # Doesn't matter for now as the SRC course guide is pretty small and I have free time
+                    head = page.find("a", {"name": info[1][1:]})
+
+                    if head is not None:
+                        # Also somewhat inefficient code, but I'm not going to change it because it would
+                        # take too much effort to find something to pander towards our horseshit course guide
+                        current = head.find_next("table").find_next("td").find_next("td")
+                        desc = ""
+
+                        while current.find("table") is None:
+                            desc += current.text.strip("\n") + "  "
+                            current = current.find_next("td")
+
+                        course["Description"] = re.sub(" [ ]+", "  ", desc)
+                    else:
+                        course["Description"] = "Not available"
+
+                else:
+                    course[indexes[index]] = retrieve_info(inner, index)
 
             course["Subject"] = subjects[course["Short"].split()[0]]
 
             if check_duplicate(course, courses):
                 courses.append(course)
         else:
+            count += 1
             continue
+
+        count += 1
+        if count % 20 == 0:
+            print("Catalog: " + str(round(100*count/len(rows))) + "%")
 
     return courses
 
@@ -75,7 +106,7 @@ def get_books(courses):
             course["Books"] = None
 
         if i % 20 == 0:
-            print(str(i) + "/" + str(len(courses)))
+            print("Books: " + str(i) + "/" + str(len(courses)))
 
 
 def write_json(filename, courses):
@@ -89,12 +120,13 @@ def main():
 
     subjects = get_subjects(subjects)
     courses = get_courses(courselink, subjects)
+
     get_books(courses)
 
     for course in courses:
         print(course)
 
-    write_json("courses.json", courses)
+    write_json("coursesUpdated.json", courses)
 
 
 if __name__ == "__main__":
